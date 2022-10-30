56ab567caec31e33647a553be0f31cea01789731
alfonsosergio <alfonsosergio.sa@gmail.com>
2022-10-30 12:20:58 -0300

    Con comentarios

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-/* Simulamos con estas variables que el cliente no existe y no tiene un presupuesto */
+/* Simulamos con esta variables que no tenemos un presupuesto para un cliente y el monto necesario para aprobar el presupuesto*/
 let presupuesto = false;
-let cliente = false;
 let aprobado = 100000;
 
+
+/* Al cumplirse la condición vamos a confirmar que queremos cargar un presupuesto*/
 while (presupuesto === false) {
     presupuesto = prompt("Escribir 'SI' para crear presupuesto" ).toUpperCase();
     
-            
+    /* En esta sección cargamos los datos del presupuesto, si la opción que se carga no es la correcta se lanza una alerta. */
     if (presupuesto === "SI") {
         cliente = prompt("Pasar nombre del cliente");
         let precioAntenaAp = parseInt(prompt("Pasar precio de la antena AP"));
@@ -16,7 +17,7 @@ while (presupuesto === false) {
 
         let total = precioAntenaAp + precioAntenaCpe + precioGeneral + precioTransporte;
 
-        
+        /* Con este condicional vamos a aprobar el presupuesto en caso contrario vamos a tener que completar todo el formulario otra vez. */
         if (total < aprobado ) {
             alert("Presupuesto DESAPROBADO");
             presupuesto = false;
